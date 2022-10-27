298ab2b87e960f04a8c295a3988b1efc757735c2
sydali <aboubakardalitraore@gmail.com>
2022-10-27 15:46:12 +0200

    Creation de la classe tondeuses

--- src/main/scala/projetfinal/tondeuses.scala
+++ src/main/scala/projetfinal/tondeuses.scala
@@ -1,12 +1,11 @@
 package projetfinal
 
-class tondeuses (var x: Int, var y: Int, dir: Char ){
-  def avancer :
-  def deplacer(choice: Int): String = choice match {
+class tondeuses (var x: Int, var y: Int, dir: String ){
+  //def avancer :
+  //def deplacer(choice: Int): String = choice match
 }
 
 object Run extends App  {
   val tondeuse1 = new tondeuses (1, 2, "N")
   val tondeuse2 = new tondeuses (3, 3, "E")
-
 }
